7e896b7b8d9604b09c55b03c8585db01abb24857
The3Legend <durdvlad@gmail.com>
2021-01-23 19:17:11 +0200

    receive request

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import api from './sarvices/apiService'
+
+api.countries().then(res => console.log(res))
--- src/js/sarvices/apiService.js
+++ src/js/sarvices/apiService.js
@@ -5,8 +5,27 @@ class Api {
   constructor(config) {
     this.url = config.url;
   }
+  async countries() {
+    try {
+      const respons =await axios.get(`${this.url}/countries`)
+      console.log(respons)
+    } catch (error) {
+      console.log(error)
+      return Promise.reject(error)
+    }
+  }
+  async cities() {
+    try {
+      const respons =await axios.get(`${this.url}/cities`)
+      console.log(respons)
+    } catch (error) {
+      console.log(error)
+      return Promise.reject(error)
+    }
+  }
+  async prices(params) {}
 }
 
-const api = new Api(config)
+const api = new Api(config);
 
-export default api
+export default api;
