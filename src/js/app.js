7b817c8f2972ea3f9c224d75d7158c485ef5199b
The3Legend <durdvlad@gmail.com>
2021-01-25 22:38:40 +0200

    add styles

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
+import "../css/style.css";
+import "./plagins";
 import locations from "./store/locations";
 
 locations.init().then((res) => {
   console.log(res);
   console.log(locations);
-  console.log(locations.getCitiesByCountriesCode('PE'))
+  console.log(locations.getCitiesByCountriesCode("PE"));
 });
